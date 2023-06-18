857d5a8a181c59e020f69e067495f66f07259c80
Xalios <mastep@yandex.ru>
2023-06-18 22:14:59 +0300

    remove headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,10 @@
 import pytest
 from selene import browser
-from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
 def chrome_browser():
     browser.config.base_url = 'https://demoqa.com'
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options
     browser.config.window_height = 1920
     browser.config.window_width = 1080
 
