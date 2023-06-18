bf0c8d3459d9240958b1815eae56ef9cf695f0ea
Xalios <mastep@yandex.ru>
2023-06-18 22:06:18 +0300

    add headless option

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,10 @@ from selenium import webdriver
 def chrome_browser():
     browser.config.base_url = 'https://demoqa.com'
     driver_options = webdriver.ChromeOptions()
-    # driver_options.add_argument('--headless')
+    driver_options.add_argument('--headless')
     browser.config.driver_options = driver_options
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
 
     yield
 
