5b0a9a8e3683860996c9fd23d9cb7b48a9a7e257
Xalios <mastep@yandex.ru>
2023-06-19 09:58:55 +0300

    Change fixture browser resolution

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ from selene import browser
 @pytest.fixture(scope='function', autouse=True)
 def chrome_browser():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_height = 1920
-    browser.config.window_width = 1080
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
 
     yield
 
